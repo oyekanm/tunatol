9249e41504d974ea3addc9499eade9456465e26f
Boluwatife Oyekanmi <enitanboluwatife5@gmail.com>
2024-11-26 01:21:34 +0100

    change eslint config

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 export { default as deleteFirebaseFile } from "./deleteFirebaseFiles";
 export { default as formatDate } from "./dateFormatter";
-export { default as cleanupUnusedFiles } from "./cleanupStorage";
+// export { default as cleanupUnusedFiles } from "./cleanupStorage";
 export { default as generateSecretKey } from "./generateSecretKey";
 export { adminStorage, adminAuth } from "./firebase-admin";
 export { default as CacheKeys } from "./cacheKeys";
+export { authOptions } from "./authOptions"
